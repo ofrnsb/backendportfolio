3998c8bbd275c36bc9d4e7485bdc64c5c6d08517
b <ofbutar@gmail.com>
2023-02-27 17:14:32 +0700

    set error message for login service (invalid username and/or password)

--- src/main/java/com/portfoliobackend/pb/Controllers/login.java
+++ src/main/java/com/portfoliobackend/pb/Controllers/login.java
@@ -1,5 +1,8 @@
 package com.portfoliobackend.pb.Controllers;
 
+import com.portfoliobackend.pb.DTO.responseData;
+import com.portfoliobackend.pb.Models.Entities.registerEntity;
+import com.portfoliobackend.pb.Models.Repositories.loginRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,9 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.portfoliobackend.pb.Models.Entities.registerEntity;
-import com.portfoliobackend.pb.Models.Repositories.loginRepo;
 
 @RestController
 @RequestMapping("/login")
@@ -19,23 +19,22 @@ public class login {
   private loginRepo loginRepo;
 
   @PostMapping
-  public ResponseEntity<String> loginRequest(@RequestBody registerEntity user) {
+  public ResponseEntity<responseData<String>> loginRequest(
+    @RequestBody registerEntity user
+  ) {
+    responseData<String> response = new responseData<>();
     registerEntity existingUser = loginRepo
       .findByUsernameAndPassword(user.getUsername(), user.getPassword())
       .orElse(null);
     if (existingUser == null) {
-      return new ResponseEntity<>(
-        "Invalid username or password",
-        HttpStatus.UNAUTHORIZED
-      );
+      response.setMessage("Username or Password is incorrect");
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     } else if (!existingUser.getPassword().equals(user.getPassword())) {
-      return new ResponseEntity<>(
-        "Invalid username or password",
-        HttpStatus.UNAUTHORIZED
-      );
+      response.setMessage("Username or Password is incorrect");
+      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(response);
     } else {
-      return new ResponseEntity<>("Login successful", HttpStatus.OK);
+      response.setMessage("Login Success");
+      return ResponseEntity.ok(response);
     }
   }
 }
-
