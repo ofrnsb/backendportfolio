3d186e719275ee2d475f5bb4fb61e47e3a402131
b <ofbutar@gmail.com>
2023-02-28 13:17:08 +0700

    add CORS for login Service

--- src/main/java/com/portfoliobackend/pb/Controllers/login.java
+++ src/main/java/com/portfoliobackend/pb/Controllers/login.java
@@ -6,6 +6,7 @@ import com.portfoliobackend.pb.Models.Repositories.loginRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/login")
+@CrossOrigin(origins = "*")
 public class login {
 
   @Autowired
