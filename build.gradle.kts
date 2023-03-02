cc6ed747aab6230a40adf067afa396db6db12d78
b <ofbutar@gmail.com>
2023-03-02 23:47:03 +0700

    path class

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,6 +46,6 @@ tasks.withType<Javadoc>() {
 }
 tasks.withType<Jar> {
     manifest {
-        attributes["Main-Class"] = "com/portfoliobackend/pb"
+        attributes["Main-Class"] = "com/portfoliobackend/pb/PbApplication"
     }
 }
