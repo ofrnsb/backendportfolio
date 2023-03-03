069f5b247f3218590226be5a4274fc1c5d8a738b
b <ofbutar@gmail.com>
2023-03-03 17:16:01 +0700

    add dev to .jar

--- src/main/java/com/portfoliobackend/pb/PbApplication.java
+++ src/main/java/com/portfoliobackend/pb/PbApplication.java
@@ -1,8 +1,10 @@
 package com.portfoliobackend.pb;
 
+import com.portfoliobackend.pb.Utils.CustomErrorAttributes;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -16,6 +18,11 @@ public class PbApplication {
   @Bean
   public ModelMapper modelMapper() {
     return new ModelMapper();
+  }
+
+  @Bean
+  public DefaultErrorAttributes errorAttributes() {
+    return new CustomErrorAttributes();
   }
 
   @Bean
