eeac6e4db8d6b462e280d3133f59dc457c9721a4
b <ofbutar@gmail.com>
2023-03-10 16:01:26 +0700

    fix controller for Category

--- src/main/java/com/backend/portfolio/Controllers/MoneyManagement/CategoryController.java
+++ src/main/java/com/backend/portfolio/Controllers/MoneyManagement/CategoryController.java
@@ -1,8 +1,8 @@
 package com.backend.portfolio.Controllers.MoneyManagement;
 
 import com.backend.portfolio.DTO.responseData;
-import com.backend.portfolio.Models.Entities.MoneyManagement.KindEntity;
-import com.backend.portfolio.Services.MoneyManagement.kindService;
+import com.backend.portfolio.Models.Entities.MoneyManagement.CategoryEntity;
+import com.backend.portfolio.Services.MoneyManagement.categoryService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,23 +13,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/kind")
+@RequestMapping("/Category")
 @CrossOrigin("*")
-public class KindController {
+public class CategoryController {
 
   @Autowired
   private ModelMapper modelMapper;
 
   @Autowired
-  private kindService kindService;
+  private categoryService categoryService;
 
   @PostMapping
   public ResponseEntity<responseData<String>> createKind(
-    @RequestBody KindEntity kindEntity
+    @RequestBody CategoryEntity categoryEntity
   ) {
     responseData<String> response = new responseData<>();
 
-    kindService.addKind(modelMapper.map(kindEntity, KindEntity.class));
+    categoryService.addCategory(
+      modelMapper.map(categoryEntity, CategoryEntity.class)
+    );
 
     response.setMessage("Login Success");
     return ResponseEntity.ok(response);
